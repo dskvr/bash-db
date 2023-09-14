b2009eb88ab8e8e8b5f67cf1361e121838d8113e
Sandwich <299465+dskvr@users.noreply.github.com>
2023-09-14 14:26:47 +0200

    revert changes
    Just stick to command-line tool since bpkg doesn't have good solution for dependency resolution in both global and source cases.

--- db.sh
+++ db.sh
@@ -104,36 +104,28 @@ help () {
    exit 0
 }
 
-if [[ ${BASH_SOURCE[0]} != $0 ]]; then
-   # Source
-   export -f get
-   export -f list 
-   export -f last 
-   export -f put 
-   export -f delete 
-else
-   # Start
-   if [[ -z "$1" || "$1" == "--help" ]];
-   then
-      help
-      exit 0
-   fi
-   
-   if [[ "$1" =~ (get|list|last|put|delete) ]];
-   then
-      if [[ -z "$2" ]];
-      then
-         echo "No database provided"
-         exit 2
-      else
-         "$@"
-      fi
-   else
-      echo method "'$1'" not found
-      help
-      exit 1
-   fi
+# Start
+if [[ -z "$1" || "$1" == "--help" ]];
+then
+   help
+   exit 0
 fi
+
+if [[ "$1" =~ (get|list|last|put|delete) ]];
+then
+   if [[ -z "$2" ]];
+   then
+      echo "No database provided"
+      exit 2
+   else
+      "$@"
+   fi
+else
+   echo method "'$1'" not found
+   help
+   exit 1
+fi
+
 
 
 
